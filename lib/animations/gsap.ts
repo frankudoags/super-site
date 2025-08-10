8f6644333803211100e7fd26ebf6c777358eabf1
frankudoags <frankudoags.com>
2025-08-10 17:33:34 +0100

    nits

--- lib/animations/gsap.ts
+++ lib/animations/gsap.ts
@@ -3,7 +3,8 @@ import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(MotionPathPlugin,MorphSVGPlugin,ScrollTrigger);
+gsap.registerPlugin(MotionPathPlugin,MorphSVGPlugin,ScrollTrigger, useGSAP);
 
-export { gsap };
+export { gsap, useGSAP};
